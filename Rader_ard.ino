70248b60f883064dc0d123327c20759983eff7a9
Asif Ahmed Neloy <asif.neloy@northsouth.edu>
2017-11-02 01:06:25 +0600

    Update Rader_ard.ino

--- Rader_ard.ino
+++ Rader_ard.ino
@@ -14,7 +14,7 @@ void setup() {
   myServo.attach(11); // Defines on which pin is the servo motor attached
 }
 void loop() {
-  // rotates the servo motor from 15 to 165 degrees
+  // rotates the servo motor from 0 to 180 degrees
   for(int i=0;i<=179;i++){  
   myServo.write(i);
   delay(35);
@@ -25,7 +25,7 @@ void loop() {
   Serial.print(distance); // Sends the distance value into the Serial Port
   Serial.print("."); // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
   }
-  // Repeats the previous lines from 165 to 15 degrees
+  // Repeats the previous lines from 180 to 0 degrees
   for(int i=179;i>0;i--){  
   myServo.write(i);
   delay(35);
